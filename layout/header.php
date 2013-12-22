54c86d45d85b3d55d6658ecb5c7d1ce427f15bef
lqp2792 <lqp.2792@gmail.com>
2013-12-22 09:16:57 +0700

    as

--- layout/header.php
+++ layout/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="pl" xml:lang="pl">
 <head>
-    <base href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/'?>">
+    <base href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/'?>" />
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="author" content="Le Quang Phu" />
     <title><?php echo $page_title?></title>
@@ -23,7 +23,7 @@
             <div class="left">
                 <?php
                     if(isset($_SESSION['username'])) {
-                        echo '<p>Welcome, <strong>'.$_SESSION['username'].'</strong> [ <a href="">logout</a> ]</p>';
+                        echo '<p>Welcome, <strong>'.$_SESSION['username'].'</strong> [ <a href="Logout/">logout</a> ]</p>';
                     } else {
                         echo '<p>English Test, Yolo !</p>';
                     }
